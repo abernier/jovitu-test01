17146c81700e65d759235a57217bdf6d304eddb0
abernier <antoine.bernier@gmail.com>
2020-12-10 14:12:22 +0100

    multiple images

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ console.log('coucou')
 
 const $overlay = document.querySelector('.overlay')
 const $img = document.querySelector('.img')
-const $tds = document.querySelectorAll('td')
+const $tds = document.querySelectorAll('[data-img]')
 
-$overlay.onclick = e => $overlay.classList.remove('active')
+$overlay.onclick = e => {
+  $img.src = ""
+  $overlay.classList.remove('active')
+}
 
-$tds.forEach($td => $td.addEventListener('click', e => $overlay.classList.add('active')))
+$tds.forEach($td => $td.addEventListener('click', e => {
+  $img.src = $td.dataset.img
+  $overlay.classList.add('active')
+}))
